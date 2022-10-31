2debfb7125e1b7fae9a6e104c843eb1bb3e2b789
Lucas <lcseferreira@hotmail.com>
2022-10-31 19:24:30 -0300

    RestTemplate POST

--- src/main/java/edu/lucas/DDSpringBootEssentials/client/SpringClient.java
+++ src/main/java/edu/lucas/DDSpringBootEssentials/client/SpringClient.java
@@ -3,8 +3,7 @@ package edu.lucas.DDSpringBootEssentials.client;
 import edu.lucas.DDSpringBootEssentials.domain.Movie;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
@@ -23,8 +22,25 @@ public class SpringClient {
         log.info(Arrays.toString(movies));
 
         //@formatter:off
-        ResponseEntity<List<Movie>> exchange = new RestTemplate().exchange("http://localhost:8080/movies/all", HttpMethod.GET, null, new ParameterizedTypeReference<>() {});
+        ResponseEntity<List<Movie>> exchange = new RestTemplate().exchange("http://localhost:8080/movies/all", HttpMethod.GET, null, new ParameterizedTypeReference<>() {
+        });
         //@formatter:on
         log.info(exchange.getBody());
+
+        Movie sextaFeira13 = Movie.builder().name("Sexta-Feira 13").build();
+        ResponseEntity<Movie> postEntity = new RestTemplate().postForEntity("http://localhost:8080/movies", sextaFeira13, Movie.class);
+        log.info(postEntity);
+
+        Movie aHoraDoPesadelo = new RestTemplate().postForObject("http://localhost:8080/movies", Movie.builder().name("A hora do Pesadelo").build(), Movie.class);
+        log.info(aHoraDoPesadelo);
+
+        ResponseEntity<Movie> aCasaDeCera = new RestTemplate().exchange("http://localhost:8080/movies", HttpMethod.POST, new HttpEntity<>(Movie.builder().name("A casa de cera").build(), createJsonHeader()), Movie.class);
+        log.info(aCasaDeCera);
+    }
+
+    private static HttpHeaders createJsonHeader() {
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+        return httpHeaders;
     }
 }
