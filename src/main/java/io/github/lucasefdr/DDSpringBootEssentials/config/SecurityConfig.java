8b202c05c35b94675290828aceb5f3b494da1e96
Lucas <lucas.efdrosa@gmail.com>
2022-12-13 23:11:58 -0300

    CSRF token

--- src/main/java/io/github/lucasefdr/DDSpringBootEssentials/config/SecurityConfig.java
+++ src/main/java/io/github/lucasefdr/DDSpringBootEssentials/config/SecurityConfig.java
@@ -1,23 +1,17 @@
 package io.github.lucasefdr.DDSpringBootEssentials.config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-
-import javax.sql.DataSource;
-
-import static org.springframework.security.config.Customizer.withDefaults;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 /**
  * Annotation para configuração de segurança
@@ -26,16 +20,19 @@ import static org.springframework.security.config.Customizer.withDefaults;
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((authz) -> authz
+        http.csrf().disable()
+                // csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                .authorizeHttpRequests((authz) -> authz
                         .anyRequest()
                         .authenticated())
-                .httpBasic(withDefaults());
+                .httpBasic();
+
         return http.build();
     }
 
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
-        return (web) -> web.ignoring().antMatchers("/movies/all");
+        return (web) -> web.ignoring().antMatchers();
     }
 
     @Bean
